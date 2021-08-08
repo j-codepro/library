11aba6ebb93513bbd370f1d8fbe7b95c482f3142
j-codepro <j_code@icloud.com>
2021-08-08 17:33:01 +0200

    homepage

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('.container');
+const addBook = document.querySelector('.add');
+
+let myLibrary = [];
+
+function Book() {
+    // the constructor..
+}
+
+function addBookToLibrary() {
+    addBook.addEventListener('click', () => {
+        
+    })
+}
+
+//const div = document.createElement('div');
+//container.appendChild(div).classList.add('card');
